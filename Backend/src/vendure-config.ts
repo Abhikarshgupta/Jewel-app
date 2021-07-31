c627f5737d5fcd3f9c54b4cd086b8deb55ac88a8
Abhikarsh Gupta <abg71297@gmail.com>
2021-07-31 22:16:50 +0530

    Backend Commit
    Backend server

---
+++ Backend/src/vendure-config.ts
@@ -0,0 +1,82 @@
+import {
+    dummyPaymentHandler,
+    DefaultJobQueuePlugin,
+    DefaultSearchPlugin,
+    VendureConfig,
+    NativeAuthenticationStrategy,
+} from '@vendure/core'; 
+import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
+import { AssetServerPlugin } from '@vendure/asset-server-plugin';
+import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
+import path from 'path';
+
+export const config: VendureConfig = {
+    apiOptions: {
+        port: 3000,
+        adminApiPath: 'admin-api',
+        adminApiPlayground: {
+            settings: {
+                'request.credentials': 'include',
+            } as any,
+        },// turn this off for production
+        adminApiDebug: true, // turn this off for production
+        shopApiPath: 'shop-api',
+        shopApiPlayground: {
+            settings: {
+                'request.credentials': 'include',
+            } as any,
+        },// turn this off for production
+        shopApiDebug: true,// turn this off for production
+    },
+    authOptions: {
+        shopAuthenticationStrategy: [
+            new NativeAuthenticationStrategy(),
+          ],
+        requireVerification:false,
+        superadminCredentials: {
+            identifier: 'superadmin',
+            password: 'superadmin',
+        },
+    },
+    dbConnectionOptions: {
+        type: 'mysql',
+        synchronize: true, // turn this off for production
+        logging: false,
+        database: 'new_vendure',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'admin@123',
+        migrations: [path.join(__dirname, '../migrations/*.ts')],
+    },
+    paymentOptions: {
+        paymentMethodHandlers: [dummyPaymentHandler],
+    },
+    customFields: {},
+    plugins: [
+        AssetServerPlugin.init({
+            route: 'assets',
+            assetUploadDir: path.join(__dirname, '../static/assets'),
+        }),
+        DefaultJobQueuePlugin,
+        DefaultSearchPlugin,
+        EmailPlugin.init({
+            devMode: true,
+            outputPath: path.join(__dirname, '../static/email/test-emails'),
+            route: 'mailbox',
+            handlers: defaultEmailHandlers,
+            templatePath: path.join(__dirname, '../static/email/templates'),
+            globalTemplateVars: {
+                // The following variables will change depending on your storefront implementation
+                fromAddress: '"example" <noreply@example.com>',
+                verifyEmailAddressUrl: 'http://localhost:8080/verify',
+                passwordResetUrl: 'http://localhost:8080/password-reset',
+                changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
+            },
+        }),
+        AdminUiPlugin.init({
+            route: 'admin',
+            port: 3002,
+        }),
+    ],
+};
